37c123408d454ebd235af1e1ecdc96e2a57d8ed0
ramsis-hub <77010921+ramsis-hub@users.noreply.github.com>
2026-03-14 08:16:37 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# 1. 强制使用 dnsmasq-full (解决插件冲突)
+# 1. 修复 DNSMasq 冲突
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
 
-# 2. 创建 R2S 自动扩容脚本
+# 2. 创建 R2S 自动扩容与 Swap 脚本 (写入固件内，不在编译服务器运行)
 mkdir -p files/etc/uci-defaults
 cat << 'EOF' > files/etc/uci-defaults/99-r2s-setup
 #!/bin/sh
@@ -17,6 +17,7 @@ if [ ! -e /etc/config/resize_done ]; then
         mkswap /opt/swap
         chmod 600 /opt/swap
     fi
+    
     swapon /opt/swap
     echo "/opt/swap swap swap defaults 0 0" >> /etc/fstab
     reboot
@@ -29,5 +30,5 @@ fi
 exit 0
 EOF
 
-# 3. 设置脚本执行权限
+# 3. 设置权限
 chmod +x files/etc/uci-defaults/99-r2s-setup
